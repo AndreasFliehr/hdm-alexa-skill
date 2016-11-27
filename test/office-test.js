042953e568f53569618055f18ed2cc2948d2bc13
Eric Schmidt <eric.schmidt93@gmail.com>
2016-11-27 15:02:45 +0100

    Tested messages for multiple lecturer in office.js

--- test/office-test.js
+++ test/office-test.js
@@ -5,6 +5,7 @@ var sinon = require('sinon');
 var sandbox = sinon.sandbox.create();
 var data = require('./data/office');
 var dataNoRoom = require('./data/officeNoRoom');
+var dataMultipleLecturers = require('./data/officeMultipleLecturers');
 
 describe ('office', function() {
     'use strict';
@@ -24,14 +25,21 @@ describe ('office', function() {
         expect(searchDetailsSpy.called);
     });
 
-    it('should return answer for lecturer Walter Kriha', function(done) {
-        var expected = 'Das Büro von Walter Kriha befindet sich in Raum 322';
+    it('should return answer for lecturer', function(done) {
+        var expected = 'Das Büro von Walter Kriha befindet sich in Raum 322.';
         testResponse('person', 'Walter Kriha', expected, data, done);
     });
 
-    it('should return answer for lecturer Thomas Pohl', function(done) {
-        var expected = 'Es existiert kein Büro für Thomas Pohl';
+    it('should return answer for lecturer with no room', function(done) {
+        var expected = 'Es existiert kein Büro für Thomas Pohl.';
         testResponse('person', 'Thomas Pohl', expected, dataNoRoom, done);
+    });
+
+    it('should return answer for multiple lectures', function(done) {
+        var expected = 'Es wurden 2 Personen gefunden:' +
+            ' Das Büro von Walter Kriha befindet sich in Raum 322.' +
+            ' Es existiert kein Büro für Thomas Pohl.';
+        testResponse('person', 'Thomas', expected, dataMultipleLecturers, done);
     });
 
     it('should provide error if client throws one', function(done) {
