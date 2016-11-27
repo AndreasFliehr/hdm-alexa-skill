f29c54474fd4570f3bd5f3013ce493c0d9c8837e
Eric Schmidt <eric.schmidt93@gmail.com>
2016-11-27 12:39:38 +0100

    Tested client callability in office.js

--- test/office-test.js
+++ test/office-test.js
@@ -1,11 +1,24 @@
 var rewire = require('rewire');
 var office = rewire('../lib/office');
 var expect = require('chai').expect;
+var sinon = require('sinon');
+var sandbox = sinon.sandbox.create();
 
 describe ('office', function() {
     'use strict';
 
+    afterEach(function() {
+        sandbox.restore();
+    });
+
     it('should be a function #office', function() {
         expect(office).to.be.a('function');
+    });
+
+    it('should call client', function(done) {
+        var searchDetailsSpy = sandbox.spy(office.__get__('client'),
+            'searchDetails');
+        office('person','Walter Kriha', done());
+        expect(searchDetailsSpy.called);
     });
 });
