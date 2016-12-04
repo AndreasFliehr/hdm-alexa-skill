05249812fb8f87955a6b2c138f2b276a8db77dad
Andreas Fliehr <afliehr@gmx.de>
2016-12-04 12:56:20 +0100

    Bugfixes in codestyle and removed not required elements

--- test/unit/lectureRoom-test.js
+++ test/unit/lectureRoom-test.js
@@ -1,18 +1,10 @@
-var rewire = require('rewire');
-var lectureRoom = rewire('../../lib/lectureRoom');
+var lectureRoom = require('../../lib/lectureRoom');
 var expect = require('chai').expect;
-var sinon = require('sinon');
-var sandbox = sinon.sandbox.create();
-var data = require('./data/lectureRoom');
 
 describe('lectureRoom', function() {
     'use strict';
 
-    afterEach(function () {
-        sandbox.restore();
-    });
-
-    it('should be a function #lectureRoom', function () {
+    it('should be a function #lectureRoom', function() {
         expect(lectureRoom).to.be.a('function');
     });
 });
