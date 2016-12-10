ab4e6d9336c1b3824507d2af881c260534227ef9
Eric Schmidt <eric.schmidt93@gmail.com>
2016-12-10 13:41:29 +0100

    Added test for ects which tests if the client returns the correct answer for a empty list of lectures;

--- test/unit/ects-test.js
+++ test/unit/ects-test.js
@@ -4,6 +4,9 @@ var sinon = require('sinon');
 var sandbox = sinon.sandbox.create();
 var ects;
 
+var ectsNoData = [
+];
+
 describe ('ects', function() {
     'use strict';
 
@@ -25,4 +28,21 @@ describe ('ects', function() {
         expect(searchDetailsSpy.calledWithExactly(
             'Machine-Learning', done()));
     });
+
+    it('should return answer if no lecture was found', function(done) {
+        var expected = 'Ich habe keine Vorlesung mit diesem Namen gefunden.';
+        testResponse('invalid lecture', expected, ectsNoData, done);
+    });
 });
+
+function testResponse(lecture, expected, dataMock, done) {
+    'use strict';
+
+    sandbox.stub(ects.__get__('client'), 'searchDetails')
+        .callsArgWith(2, null, dataMock);
+
+    ects(lecture, function(err, response) {
+        expect(response).to.equal(expected);
+        done();
+    });
+}
