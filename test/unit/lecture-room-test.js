82388034a6c2924b5554d299e6c348dc2013ce5d
Andreas Fliehr <Andi@Andreass-MBP.fritz.box>
2016-12-11 13:58:08 +0100

    Added test for lectureRoom which calls the client

--- test/unit/lecture-room-test.js
+++ test/unit/lecture-room-test.js
@@ -1,10 +1,28 @@
-var lectureRoom = require('../../lib/lectureRoom');
+var rewire = require('rewire');
 var expect = require('chai').expect;
+var sinon = require('sinon');
+var sandbox = sinon.sandbox.create();
+var lectureRoom;
 
 describe('lectureRoom', function() {
     'use strict';
 
+    beforeEach(function() {
+        lectureRoom = rewire('../../lib/lectureRoom');
+    });
+
+    afterEach(function() {
+        sandbox.restore();
+    });
+
     it('should be a function #lectureRoom', function() {
         expect(lectureRoom).to.be.a('function');
+    });
+
+    it('should call client', function(done) {
+        var searchDetailsSpy = sandbox.spy();
+        lectureRoom.__set__('client', {searchDetails: searchDetailsSpy});
+        expect(searchDetailsSpy.calledWithExactly('System ' +
+            'Engineering', done()));
     });
 });
