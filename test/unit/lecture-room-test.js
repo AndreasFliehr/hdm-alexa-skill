4bb7389d3c7622723f8a9457ef53d7a61d6b79c5
Andreas Fliehr <Andi@Andreass-MBP.fritz.box>
2016-12-11 23:09:21 +0100

    Added test for lectureRoom which tests to return answer for single lecture

--- test/unit/lecture-room-test.js
+++ test/unit/lecture-room-test.js
@@ -4,6 +4,14 @@ var sinon = require('sinon');
 var sandbox = sinon.sandbox.create();
 var lectureRoom;
 
+var lectureRoomSingleData = [
+    {
+        name: 'System Engineering und Management',
+        room: '141'
+    }
+];
+
+
 describe('lectureRoom', function() {
     'use strict';
 
@@ -22,7 +30,27 @@ describe('lectureRoom', function() {
     it('should call client', function(done) {
         var searchDetailsSpy = sandbox.spy();
         lectureRoom.__set__('client', {searchDetails: searchDetailsSpy});
-        expect(searchDetailsSpy.calledWithExactly('System ' +
-            'Engineering', done()));
+        expect(searchDetailsSpy.calledWithExactly(
+            'System Engineering', done()));
     });
+
+    it('should return answer for single lecture', function(done) {
+        var expected = 'System Engineering und Management findet ' +
+            'in Raum 141 statt.';
+        testResponse('System Engineering', expected,
+            lectureRoomSingleData, done);
+    });
+
 });
+
+function testResponse(lecture, expected, dataMock, done) {
+    'use strict';
+
+    sandbox.stub(lectureRoom.__get__('client'), 'searchDetails')
+        .callsArgWith(2, null, dataMock);
+
+    lectureRoom(lecture, function(err, response) {
+        expect(response).to.equal(expected);
+        done();
+    });
+}
