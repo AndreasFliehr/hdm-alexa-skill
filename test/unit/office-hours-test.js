c99f56ec9aea32642a844a4215da34cd9de68d2d
Andreas Fliehr <Andi@Andreass-MBP.fritz.box>
2016-12-19 23:02:27 +0100

    Refactored variable in office-hours-test

--- test/unit/office-hours-test.js
+++ test/unit/office-hours-test.js
@@ -1,10 +1,10 @@
 var expect = require('chai').expect;
-var office = require('../../lib/lecturer');
+var lecturer = require('../../lib/lecturer');
 
 describe ('officeHours', function() {
     'use strict';
 
     it('should be a function #officeHours', function() {
-        expect(office.officeHours).to.be.a('function');
+        expect(lecturer.officeHours).to.be.a('function');
     });
 });
