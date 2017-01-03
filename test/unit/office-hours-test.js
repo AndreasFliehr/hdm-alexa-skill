c8f8836e9030195fa01a0e4a81f8103b61c7fbec
jooseem <jose@egasmon.com>
2017-01-03 11:03:00 +0100

    added officehours and first test

--- test/unit/office-hours-test.js
+++ test/unit/office-hours-test.js
@@ -1,10 +1,29 @@
+var rewire = require('rewire');
+var officeHours = rewire('../../lib/officeHours');
 var expect = require('chai').expect;
-var lecturer = require('../../lib/lecturer');
+var sinon = require('sinon');
+var sandbox = sinon.sandbox.create();
 
-describe ('officeHours', function() {
+describe('officeHours', function() {
     'use strict';
 
     it('should be a function #officeHours', function() {
-        expect(lecturer.officeHours).to.be.a('function');
+        // 'expect(lecturer.officeHours).to.be.a('function');
+    });
+
+    it('should get a response', function(done) {
+        var data = {
+            name: 'Sabine Ghellal',
+            officehours: 'Do 14:15-16:15'
+        };
+        sandbox.stub(officeHours.__get__('client'), 'searchDetails')
+        .callsArgWith(2, null, data);
+
+        officeHours('Sabine Ghellal', function(err, response) {
+                expect(err).to.equal(null);
+                expect(response).to.equal('Sie hat am Donnerstag von' +
+                ' 14 Uhr 15 bis 16 Uhr 15 Vorlesung');
+                done();
+            });
     });
 });
